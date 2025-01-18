196a802968489cee5924e66080442f8938882727
kumarsarthak780 <sakshikumarimadhuram@gmail.com>
2025-01-18 11:59:52 +0530

    another version

--- server.js
+++ server.js
@@ -2,22 +2,53 @@ const express= require('express');
 const app= express();
 const db = require('./db'); 
 require('dotenv').config();
-
+const passport= require('passport')
+const LocalStrategy= require('passport-local').Strategy;
 const Person= require("./models/person.js")
 const Menu= require("./models/menu.js")
 
 
 const bodyparser= require('body-parser')
 app.use(bodyparser.json());
+app.use(passport.initialize());
 
 
+
+
+//Middleware Function
+
+passport.use(new LocalStrategy(async (USERNAME, password, done)=>{
+    try{
+        console.log('Received credentials', USERNAME, password);
+        const user= await Person.findOne({username: USERNAME});
+        if(!user){
+            return done(null, false, {message: 'Incorrect username'});
+        }
+        const isPasswordMatch= user.password== password?true: false;
+        if(isPasswordMatch){
+            return done(null, user);
+
+        }
+        else{
+            return done(null, false, {message: 'Incorrect Password'})
+        }
+    }
+    catch(err){
+
+    }
+}))
+
+const logRequest= (req, res, next)=>{
+    console.log(`${new Date().toLocaleString()} Request Made to: ${req.originalUrl}`);
+    next();
+}
 
 const PORT= process.env.PORT || 9000;
 app.listen(9000, ()=>{
     console.log('Server is running on port 9000')
 });
-
-app.get('/', (req, res)=>{
+app.use(logRequest);
+app.get('/', function (req, res){
     res.send('Welcome to my hotel')
 })
 
